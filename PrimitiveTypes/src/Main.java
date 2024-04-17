df0900dc2f136214e6774c3f94c6be0f43617ccf
athif-work <164720365+athif-work@users.noreply.github.com>
2024-04-17 16:04:41 +0530

    Add files via upload

---
+++ PrimitiveTypes/src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+        byte age=10;
+        long viewsCount=3_123_456_789L;
+        float price=9.99F;
+        char letter='A';
+        boolean isEligible=false;
+
+
+    }
+}
