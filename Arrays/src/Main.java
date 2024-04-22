ded5ebfca627047526f416c976d78c475049ea21
athif-work <164720365+athif-work@users.noreply.github.com>
2024-04-22 20:45:49 +0530

    Add files via upload

---
+++ Arrays/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        int[] numbers={1,2,3,5,1,7};
+        Arrays.sort(numbers);
+        System.out.println(Arrays.toString(numbers));
+
+
+    }
+}
