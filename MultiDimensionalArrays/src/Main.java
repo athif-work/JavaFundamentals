80fbd86760ce90bb13e5e2a8f11409f0c99e1fb7
athif-work <164720365+athif-work@users.noreply.github.com>
2024-04-22 21:04:14 +0530

    Add files via upload

---
+++ MultiDimensionalArrays/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] numbers=new int[2][3];
+        numbers[0][0]=1;
+        System.out.println(Arrays.deepToString(numbers));
+        int[][] numbers_1={{1,2,3},{4,5,6}};
+        System.out.println(Arrays.deepToString(numbers_1));
+    }
+}
