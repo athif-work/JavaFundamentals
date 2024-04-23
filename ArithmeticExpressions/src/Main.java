5f2159d0856c09406f8431c8e5be5bdca40ff2fd
athif-work <164720365+athif-work@users.noreply.github.com>
2024-04-23 11:53:13 +0530

    Add files via upload

---
+++ ArithmeticExpressions/src/Main.java
@@ -0,0 +1,23 @@
+
+public class Main {
+    public static void main(String[] args) {
+    int result=1+1;
+        System.out.println(result);
+        double result1=(double)10/(double) 3;
+        System.out.println(result1);
+        int x=1;
+        int y=++x;
+        System.out.println(x);
+        System.out.println(y);
+        //increment
+        x=1;
+        y=x++;
+        System.out.println(x);
+        System.out.println(y);
+        //augmented assignment operator:
+        int a=2;
+        a+=3;
+        System.out.println(a);
+
+    }
+}
