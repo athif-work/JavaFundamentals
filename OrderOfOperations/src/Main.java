915a732a9fac7e3fb44c759cbdc0ec80b2c2595e
athif-work <164720365+athif-work@users.noreply.github.com>
2024-04-23 11:56:54 +0530

    Add files via upload

---
+++ OrderOfOperations/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+        //()
+        //* /
+        //+ -
+
+    int result=10+2*2;
+        System.out.println(result);
+        int result1=(10+2)*2;
+        System.out.println(result1);
+    }
+}
