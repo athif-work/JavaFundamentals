44267b25aeb77f013d529732fa0f496dc95846b6
athif-work <164720365+athif-work@users.noreply.github.com>
2024-04-29 23:52:17 +0530

    Add files via upload

---
+++ ternaryOperator/src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+    int income=123_000;
+    String className=income>120_000 ? "First" : "Economy";
+        System.out.println(className);
+    }
+}
