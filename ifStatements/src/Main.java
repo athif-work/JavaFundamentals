a03cefbf87910aec57cd9ad05ccc2421c8c59f14
athif-work <164720365+athif-work@users.noreply.github.com>
2024-04-29 23:41:52 +0530

    Add files via upload

---
+++ ifStatements/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int temp=2;
+        if(temp>30){
+            System.out.println("Its hot");
+            System.out.println("drink plenty of water");
+        }
+        else if (temp>20){
+            System.out.println("Beautiful day");
+        }
+        else
+            System.out.println("cold day");
+    }
+}
