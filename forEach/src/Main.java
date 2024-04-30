cc493c692df4a2f1e331e9c7e7295e1b2436ee78
athif-work <164720365+athif-work@users.noreply.github.com>
2024-04-30 09:04:58 +0530

    Add files via upload

---
+++ forEach/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+    String[] fruits= {"apple", "mango","orange"};
+    for(int i=0;i< fruits.length;i++)
+        System.out.println(fruits[i]);
+    for(String fruit:fruits)
+        System.out.println(fruit);
+    }
+}
