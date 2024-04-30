88ba0d3cc55670b0e6d113781bb5647d03ed0db7
athif-work <164720365+athif-work@users.noreply.github.com>
2024-04-30 08:59:37 +0530

    Add files via upload

---
+++ breakAndContinue/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+                String input="";
+                Scanner scanner=new Scanner(System.in);
+                while(true){
+                    System.out.print("Input: ");
+                    input= scanner.next().toLowerCase();
+                    if(input.equals("pass"))
+                        continue;
+                    if(input.equals("quit"))
+                        break;
+                    System.out.println(input);
+
+
+                }
+
+
+    }
+}
