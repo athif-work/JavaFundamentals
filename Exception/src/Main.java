733b71520c90576694fa73411a746bd6cac2a266
athif-work <164720365+athif-work@users.noreply.github.com>
2024-06-04 16:19:34 +0530

    Add files via upload

---
+++ Exception/src/Main.java
@@ -0,0 +1,28 @@
+
+public class Main {
+    public static void main(String[] args) {
+        try{
+            int a=10;
+            int b=0;
+            int c=a/b;
+        }
+        catch (ArithmeticException e){
+            System.out.println("Cannot divide a number by zero");
+        }
+        try{
+            int num=Integer.parseInt("hai");
+            System.out.println(num);
+
+        }
+        catch (NumberFormatException e){
+            System.out.println("String cannot be integer");
+        }
+        try{
+            int a[]=new int[5];
+            a[7]=10;
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Array out of bounds");
+        }
+
+    }
+}
